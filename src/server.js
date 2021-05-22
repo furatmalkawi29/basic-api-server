60e81f526600d78e8d32415ab12808a109bce47f
Furat Malkawi <78353979+furatmalkawi29@users.noreply.github.com>
2021-05-22 16:36:19 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const clothesRoutes = require('./routes/clothes.js');
 
 //use
 app.use(express.json()); /*body parser //allows using body in 
-methods who doesnt have body / post/put/patch */
+methods who doesnt have (request body) / post/put/patch */
 app.use(morgan('dev')); // console each req / debugging
 app.use(cors());
 
